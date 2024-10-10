31490287a7336b6ed33b49d75b655975e9111c0b
gabrieusu0 <gabriel.osantos@sptech.school>
2024-10-09 23:08:36 -0300

    Adicionando INSERTS e JOIN no script do BD

--- Banco de Dados/BrightSight.sql
+++ Banco de Dados/BrightSight.sql
@@ -34,26 +34,42 @@ numero INT,
 FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
-INSERT INTO local_sensor VALUES
-	(1, 1, '12345678'),
-    (2, 1, '87654321'),
-    (3, 2, '13579753'),
-    (4, 2, '24686420'),
-    (5, 3, '08642468'),
-    (6, 3, '12348765'),
-    (7, 4, '43215678'),
-    (8, 5, '32145698');
+INSERT INTO local_sensor (fkEmpresa, CepLocal, regiao, estado, cidade, numero)
+VALUES 
+(1, '01001000', 'Centro', 'SP', 'São Paulo', 100),
+(2, '20040010', 'Zona Sul', 'RJ', 'Rio de Janeiro', 200),
+(3, '80020000', 'Centro', 'PR', 'Curitiba', 300),
+(4, '30150000', 'Lagoa', 'MG', 'Belo Horizonte', 400),
+(4, '73150000', 'Passo Fundo', 'GO', 'Brasilia', 600),
+(5, '40010010', 'Jardim', 'BA', 'Salvador', 500);
     
 SELECT * FROM local_sensor;
     
 CREATE TABLE quadrante (
 	idQuadrante INT,
     fkLocal INT,
-    potenciaIdeal FLOAT,
+    potenciaIdeal INT,
     posicao VARCHAR(45),
 	PRIMARY KEY (idQuadrante, fkLocal),
 	FOREIGN KEY (fkLocal) REFERENCES local_sensor (idLocal)
 );
+
+
+
+INSERT INTO quadrante (idQuadrante, fkLocal, potenciaIdeal, posicao) VALUES 
+	(1, 1, 850, 'Norte'),
+	(2, 1, 780, 'Sul'),
+	(1, 2, 900, 'Leste'),
+	(2, 2, 820, 'Oeste'),
+	(1, 3, 720, 'Sul'),
+	(2, 3, 900, 'Leste'),    
+	(1, 4, 840, 'Norte'),
+	(2, 4, 820, 'Sul'),
+	(1, 5, 720, 'Leste'),
+	(2, 5, 630, 'Norte'),
+	(1, 6, 890, 'Sul'),
+	(2, 6, 940, 'Leste');
+
 
 CREATE TABLE sensor (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
@@ -63,6 +79,22 @@ CREATE TABLE sensor (
     FOREIGN KEY (fkQuadrante, fkLocal) REFERENCES quadrante (idQuadrante, fkLocal)
 );
 
+INSERT INTO SENSOR (fkQuadrante, fkLocal, numero_serie)
+VALUES 
+	(1, 1, 'Sensor SP-Norte'),
+	(2, 1, 'Sensor SP-Sul'),
+	(1, 2, 'Sensor RJ-Leste'),
+	(2, 2, 'Sensor RJ-Oeste'),
+	(1, 3, 'Sensor SP-Sul'),
+	(2, 3, 'Sensor SP-Norte'),    
+	(1, 4, 'Sensor RJ-Leste'),
+	(2, 4, 'Sensor RJ-Norte'),
+	(1, 5, 'Sensor SP-Sul'),
+	(2, 5, 'Sensor SP-Norte'),
+	(1, 6, 'Sensor RJ-Sul'),
+	(2, 6, 'Sensor RJ-Leste');
+
+
 CREATE TABLE dados (
 	idDados INT PRIMARY KEY AUTO_INCREMENT,
     potenciaAtual INT,
@@ -71,44 +103,40 @@ CREATE TABLE dados (
 	FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
 );
 
-INSERT INTO sensor VALUES
-	(1,1),
-    (2,1),
-    (3,1),
-    (1,2),
-    (2,2),
-    (3,2),
-    (1,3),
-    (2,3),
-    (3,3),
-    (1,4),
-    (2,4),
-    (3,4),
-    (1,5),
-    (2,5),
-    (3,5);
-    
-INSERT INTO dados (potênciaAtual, horaData, fkSensor, fkLocalDados) VALUES
-(120, '2024-09-30 08:45:00', 1, 1),
-(135, '2024-09-30 09:00:00', 2, 1),
-(110, '2024-09-30 09:15:00', 3, 1),
-(145, '2024-09-30 09:30:00', 1, 2),
-(125, '2024-09-30 09:45:00', 2, 2);
+INSERT INTO dados (potenciaAtual, horaData, fkSensor) VALUES 
+	(400, '2024-10-09 10:00:00', 1),
+	(600, '2024-10-09 11:00:00', 2),
+	(900, '2024-10-09 12:00:00', 3),
+	(750, '2024-10-09 13:00:00', 4),
+	(680, '2024-10-09 14:00:00', 5),
+	(590, '2024-10-09 15:00:00', 6),
+	(550, '2024-10-09 16:00:00', 7),
+	(460, '2024-10-09 17:00:00', 8),
+	(420, '2024-10-09 18:00:00', 9),
+	(360, '2024-10-09 19:00:00', 10),
+	(280, '2024-10-09 20:00:00', 11),
+	(100, '2024-10-09 21:00:00', 12);
 
-SELECT * FROM empresa JOIN localidade ON FkEmpresa = idEmpresa;
+
+SELECT * FROM empresa JOIN local_sensor ON FkEmpresa = idEmpresa;
 
 SELECT 
-    E.nome AS Empresa, 
-    L.idLocal AS Localidade, 
-    L.CepLocal AS CEP, 
-    S.idSensor AS Sensor, 
-    D.potênciaAtual AS Potência, 
-    D.horaData AS DataHora 
-FROM empresa AS E
-JOIN localidade AS L
-ON E.idEmpresa = L.fkEmpresa
-JOIN sensor AS S 
-ON L.idLocal = S.fkLocal
-JOIN dados AS D
-ON S.idSensor = D.fkSensor AND L.idLocal = D.fkLocalDados
-WHERE E.idEmpresa = 1;
+    e.nome AS 'Nome Empresa', 
+    ls.cidade AS 'Cidade do Local do Sensor',
+    ls.estado AS 'Estado do Local do Sensor',
+    q.posicao AS 'Posicao Quadrante',
+    q.potenciaIdeal AS 'Potencia Ideal Quadrante',
+    s.numero_serie AS 'Numero Serie do Sensor',
+    d.potenciaAtual AS 'Potencia Atual do Sensor',
+    d.horaData AS 'Data e Hora da Leitura'
+FROM empresa as e                   /*nome da empresa*/
+JOIN local_sensor as ls             /*tudo do local do sensor*/
+ON e.idEmpresa = ls.fkEmpresa       /*idEmpresa na empresa = fkEmpresa em local_sensor*/
+JOIN quadrante as q                 /*quadrante para mostrar potencia e posição*/
+ON ls.idLocal = q.fkLocal           /*idLocal em local_sensor = fkLocal em quadrante*/
+JOIN sensor as s                    /*sensor para mostrar seu numero de serie*/
+ON q.idQuadrante = s.fkQuadrante AND q.fkLocal = s.fkLocal   /*Referencias as duas chaves primarias pois é composta*/
+JOIN dados as d                     /*dados para mostrar a potencia registrada e data/hora*/
+ON s.idSensor = d.fkSensor          /*idSensor em sensor = fkSensor em dados*/
+ORDER BY ls.estado;                 /*ordenar pelos estados em ordem alfabetica*/
+
