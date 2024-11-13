6dc5776ee5f8734a5b399a3636d9249471f81386
ViniDiasOF <vinicius.dsilva@sptech.school>
2024-11-13 13:09:14 -0300

    Alterando codigo_verificação

--- Site Dinâmico/src/controllers/usuarioController.js
+++ Site Dinâmico/src/controllers/usuarioController.js
@@ -28,7 +28,6 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
                                     });
                                 } else {
                                     res.status(204).json({ aquarios: [] });
