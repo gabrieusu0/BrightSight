3dfc50559a3065025909e79042101a9814bf97b7
gabrieusu0 <gabriel.osantos@sptech.school>
2024-11-13 15:57:04 -0300

    Adicionando validações já existentes no estático

--- Site Dinâmico/src/models/aquarioModel.js
+++ Site Dinâmico/src/models/aquarioModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarAquariosPorEmpresa(empresaId) {
 
-  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+  var instrucaoSql = `SELECT * FROM usuario WHERE fkEmpresa = ${empresaId}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
