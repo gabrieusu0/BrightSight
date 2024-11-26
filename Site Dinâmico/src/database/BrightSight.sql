00764c14da954d95855884d49c17aa8383b292df
gabrieusu0 <gabriel.osantos@sptech.school>
2024-11-26 10:44:36 -0300

    Adicionando Campo Telefone

--- Site Dinâmico/src/database/BrightSight.sql
+++ Site Dinâmico/src/database/BrightSight.sql
@@ -6,6 +6,7 @@ USE BrightSight;
 CREATE TABLE empresa (
 	idEmpresa INT AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(50),
+    telefone CHAR(12),
 	cnpj CHAR(14),
 	rua VARCHAR(45),
 	bairro VARCHAR(45),
@@ -16,12 +17,8 @@ CREATE TABLE empresa (
     codigoVerificacao CHAR(6)
 );
 
-INSERT INTO empresa (nome, CNPJ, rua, bairro, cidade, cep, numero, senha, codigoVerificacao) VALUES
-	('SolarTech', '12345678000199', 'Rua das Flores', 'Centro', 'São Paulo', '01001000', '100', 'Senha123', 'A1B2C3'),
-	('EcoEnergy', '98765432000188', 'Av. Paulista', 'Bela Vista', 'São Paulo', '01310000', '200', 'EcoSenha2024', 'A2B3C4'),
-	('GreenPower', '56473829000177', 'Rua Verde', 'Jardim das Águas', 'Curitiba', '80010000', '300', 'Green2024', 'A3B4C5'),
-	('EnergiaLimpa', '11223344000166', 'Rua da Luz', 'Alto da Lapa', 'São Paulo', '05050000', '150', 'LuzSegura2024', 'A4B5C6'),
-	('SolEnergia', '22334455000155', 'Av. Solar', 'Sol Nascente', 'Belo Horizonte', '30110000', '250', 'SolForte2024', 'A5B6C7');
+INSERT INTO empresa (nome, telefone, CNPJ, rua, bairro, cidade, cep, numero, senha, codigoVerificacao) VALUES
+	('SolarTech', '11940028922', '12345678000199', 'Rua das Flores', 'Centro', 'São Paulo', '01001000', '100', 'Senha123', 'A1B2C3');
     
 CREATE TABLE usuario (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -45,14 +42,9 @@ numero INT,
 FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
-INSERT INTO local_sensor (fkEmpresa, CepLocal, regiao, estado, cidade, numero)
-VALUES 
-(1, '01001000', 'Centro', 'SP', 'São Paulo', 100),
-(2, '20040010', 'Zona Sul', 'RJ', 'Rio de Janeiro', 200),
-(3, '80020000', 'Centro', 'PR', 'Curitiba', 300),
-(4, '30150000', 'Lagoa', 'MG', 'Belo Horizonte', 400),
-(4, '73150000', 'Passo Fundo', 'GO', 'Brasilia', 600),
-(5, '40010010', 'Jardim', 'BA', 'Salvador', 500);
+INSERT INTO local_sensor (fkEmpresa, CepLocal, regiao, estado, cidade, numero) VALUES 
+(1, '80020000', 'Centro', 'PR', 'Curitiba', 300),
+(1, '30150000', 'Lagoa', 'MG', 'Belo Horizonte', 400);
     
 SELECT * FROM local_sensor;
     
@@ -70,16 +62,8 @@ CREATE TABLE quadrante (
 INSERT INTO quadrante (idQuadrante, fkLocal, potenciaIdeal, posicao) VALUES 
 	(1, 1, 850, 'Norte'),
 	(2, 1, 780, 'Sul'),
-	(1, 2, 900, 'Leste'),
-	(2, 2, 820, 'Oeste'),
-	(1, 3, 720, 'Sul'),
-	(2, 3, 900, 'Leste'),    
-	(1, 4, 840, 'Norte'),
-	(2, 4, 820, 'Sul'),
-	(1, 5, 720, 'Leste'),
-	(2, 5, 630, 'Norte'),
-	(1, 6, 890, 'Sul'),
-	(2, 6, 940, 'Leste');
+	(1, 2, 850, 'Leste'),
+	(2, 2, 780, 'Oeste');
     
 SELECT * FROM quadrante
 ORDER BY fkLocal;
@@ -93,20 +77,12 @@ CREATE TABLE sensor (
     FOREIGN KEY (fkQuadrante, fkLocal) REFERENCES quadrante (idQuadrante, fkLocal)
 );
 
-INSERT INTO sensor  (fkQuadrante, fkLocal, numero_serie)
-VALUES 
-	(1, 1, 'Sensor SP-Norte'),
-	(2, 1, 'Sensor SP-Sul'),
-	(1, 2, 'Sensor RJ-Leste'),
-	(2, 2, 'Sensor RJ-Oeste'),
-	(1, 3, 'Sensor SP-Sul'),
-	(2, 3, 'Sensor SP-Norte'),    
-	(1, 4, 'Sensor RJ-Leste'),
-	(2, 4, 'Sensor RJ-Norte'),
-	(1, 5, 'Sensor SP-Sul'),
-	(2, 5, 'Sensor SP-Norte'),
-	(1, 6, 'Sensor RJ-Sul'),
-	(2, 6, 'Sensor RJ-Leste');
+INSERT INTO sensor  (fkQuadrante, fkLocal, numero_serie) VALUES 
+	(1, 1, 'Sensor PR-Norte'),
+	(2, 1, 'Sensor PR-Sul'),
+    
+	(1, 2, 'Sensor MG-Leste'),
+	(2, 2, 'Sensor MG-Oeste');
     
 SELECT * FROM sensor;
 
@@ -119,10 +95,50 @@ CREATE TABLE dados (
 	FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
 );
 
+SELECT * FROM sensor;
 SELECT * FROM dados;
-
 SELECT * FROM usuario;
 SELECT * FROM empresa;
+
+SELECT * FROM sensor;
+
+
+SELECT 
+	local_sensor.cidade as 'Cidade',
+    local_sensor.estado as 'Estado',
+	quadrante.posicao as 'Posição do Quadrante',
+    sensor.idSensor as 'ID do Sensor'
+FROM empresa
+JOIN local_sensor
+	ON empresa.idEmpresa = local_sensor.fkEmpresa
+JOIN quadrante
+	ON local_sensor.IdLocal = quadrante.fkLocal
+JOIN sensor
+	ON quadrante.idQuadrante = sensor.fkQuadrante
+WHERE empresa.nome = 'EnergiaLimpa' and local_sensor.estado = 'MG';
+
+
+SELECT 
+	empresa.nome AS 'Nome da Empresa',
+	local_sensor.estado AS 'Estado',
+    local_sensor.cidade AS 'Cidade',
+	local_sensor.CepLocal AS 'CEP',
+    quadrante.posicao AS 'Posição do Quadrante',
+    sensor.numero_serie AS 'Número de Sériedo sensor',
+    dados.fkSensor as 'FK do sensor',
+    dados.horaData as 'Hora e Data',
+    dados.potenciaAtual as 'Potencia Atual'
+FROM empresa
+JOIN local_sensor
+	ON empresa.idEmpresa = local_sensor.fkEmpresa
+JOIN quadrante
+	ON local_sensor.idLocal = quadrante.fkLocal
+JOIN sensor
+	ON quadrante.idQuadrante = sensor.fkQuadrante
+JOIN dados
+	ON sensor.idSensor = dados.fkSensor
+WHERE empresa.nome = 'EnergiaLimpa' and local_sensor.cidade = 'Belo Horizonte' and quadrante.posicao = 'Sul';
+
 
 SELECT 
     e.nome AS 'Nome Empresa', 
@@ -142,3 +158,87 @@ JOIN sensor as s                    /*sensor para mostrar seu numero de serie*/
 ON q.idQuadrante = s.fkQuadrante AND q.fkLocal = s.fkLocal   /*Referencias as duas chaves primarias pois é composta*/
 JOIN dados as d                     /*dados para mostrar a potencia registrada e data/hora*/
 ON s.idSensor = d.fkSensor;          /*idSensor em sensor = fkSensor em dados*/
+
+
+
+SELECT 
+    empresa.nome AS 'Nome da Empresa',
+    local_sensor.estado AS 'Estado',
+    local_sensor.cidade AS 'Cidade',
+    local_sensor.CepLocal AS 'CEP',
+    quadrante.posicao AS 'Posição do Quadrante',
+    sensor.numero_serie AS 'Número de Série do Sensor',
+    dados.fkSensor AS 'FK do Sensor',
+    dados.horaData AS 'Mês Atual',
+    COUNT(dados.potenciaAtual) AS 'Quantidade de Registros',
+    SUM(dados.potenciaAtual) AS 'Soma de Potência no Mês'
+FROM empresa
+JOIN local_sensor
+    ON empresa.idEmpresa = local_sensor.fkEmpresa
+JOIN quadrante
+    ON local_sensor.idLocal = quadrante.fkLocal
+JOIN sensor
+    ON quadrante.idQuadrante = sensor.fkQuadrante
+JOIN dados
+    ON sensor.idSensor = dados.fkSensor
+WHERE empresa.nome = 'EnergiaLimpa' AND local_sensor.cidade = 'Belo Horizonte'  AND dados.horaData LIKE '%-09-%'
+GROUP BY 
+    empresa.nome,
+    local_sensor.estado,
+    local_sensor.cidade,
+    local_sensor.CepLocal,
+    quadrante.posicao,
+    sensor.numero_serie,
+    dados.fkSensor,
+    dados.horaData;
+    
+    
+SELECT 
+    SUM(dados.potenciaAtual) AS 'Soma Total de Potência no Mês',
+    ROUND((SUM(dados.potenciaAtual) / 30), 2) AS 'Média Mensal de Potência'
+FROM empresa
+JOIN local_sensor
+    ON empresa.idEmpresa = local_sensor.fkEmpresa
+JOIN quadrante
+    ON local_sensor.idLocal = quadrante.fkLocal
+JOIN sensor
+    ON quadrante.idQuadrante = sensor.fkQuadrante
+JOIN dados
+    ON sensor.idSensor = dados.fkSensor
+WHERE empresa.nome = 'EnergiaLimpa' 
+  AND local_sensor.cidade = 'Belo Horizonte'  
+  AND quadrante.posicao = 'Norte'
+  AND dados.horaData LIKE '%-09-%';    
+  
+  
+  
+  WITH MediaPorDia AS (
+    SELECT 
+        DATE(dados.horaData) AS 'Dia',
+        AVG(dados.potenciaAtual) AS 'MediaDiaria'
+    FROM empresa
+    JOIN local_sensor
+        ON empresa.idEmpresa = local_sensor.fkEmpresa
+    JOIN quadrante
+        ON local_sensor.idLocal = quadrante.fkLocal
+    JOIN sensor
+        ON quadrante.idQuadrante = sensor.fkQuadrante
+    JOIN dados
+        ON sensor.idSensor = dados.fkSensor
+    WHERE empresa.nome = 'EnergiaLimpa' 
+      AND local_sensor.cidade = 'Belo Horizonte'  
+      AND dados.horaData LIKE '%-09-%' -- Filtro para o mês desejado (setembro)
+    GROUP BY DATE(dados.horaData)
+),
+SomaDasMedias AS (
+    SELECT 
+        ROUND(SUM(MediaDiaria), 2) AS 'SomaDasMedias',
+        COUNT(MediaDiaria) AS 'DiasRegistrados'
+    FROM MediaPorDia
+)
+SELECT 
+    SomaDasMedias,
+    ROUND((SomaDasMedias / DiasRegistrados), 0) AS 'MediaMensal'
+FROM SomaDasMedias;
+
+
