f9c6a814efae7829c6bed1416ea566ccee49aa2a
ViniDiasOF <vinicius.dsilva@sptech.school>
2024-12-01 17:44:03 -0300

    Fundindo WEB com BOB e criação da página suporte

--- Site Dinâmico/app.js
+++ Site Dinâmico/app.js
@@ -17,7 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,17 +26,16 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######       ######                ###    #    
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##       #     #  ####  #####   #    # #   
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##        #     # #    # #    #  #   #   #
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##         ######  #    # #####   #  #     #
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##          #     # #    # #    #  #  #######
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##           #     # #    # #    #  #  #     #
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######       ######   ####  #####  ### #     # 
     \n\n\n                                                                                                 
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
@@ -45,3 +43,49 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+// Parte do BobIA
+// importando os bibliotecas necessárias
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
+
+// configurando CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json( { resultado } );
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+    // obtendo o modelo de IA
+    const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const resultado = await modeloIA.generateContent(`Sendo técnino e direto, responda com apenas um parágrafo: ${mensagem} `);
+        const resposta = await resultado.response.text();
+        
+        console.log(resposta);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
