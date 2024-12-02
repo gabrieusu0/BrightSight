b6895aa067a3db3843eaa482c8c845fb7b6c7b6b
gabrieusu0 <gabriel.osantos@sptech.school>
2024-12-02 11:05:29 -0300

    Arrumando caminhos de rota

--- Site Dinâmico/app.js
+++ Site Dinâmico/app.js
@@ -17,6 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var empresaRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +27,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/empresas", empresaRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
